9c447552d097def7358432ae676115fad0aace45
Muhammad Talha <talhayameen2005@gmail.com>
2025-07-05 16:00:36 +0500

    LeetCode Problem: 7.Reverse Integer

---
+++ task3/app.js
@@ -0,0 +1,23 @@
+// with methods
+// var n=123;
+// function reverse(){
+//   var reversed = Number(n.toString().split('').reverse().join(''));
+//   console.log(reversed); 
+// }
+// reverse();
+
+// without methods
+function reverseWithoutMethods() {
+var n = 12345;
+var reversed = 0;
+while (n > 0) {
+  let digit = n % 10;
+  reversed = reversed * 10 + digit;
+  n = Math.floor(n / 10);
+
+}
+console.log(reversed); 
+
+
+}
+reverseWithoutMethods();
