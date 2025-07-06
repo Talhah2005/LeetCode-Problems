1da690e3890c8f88634b5dce0cae89d3c0fbb185
Muhammad Talha <talhayameen2005@gmail.com>
2025-07-06 20:18:00 +0500

    LeetCode Problem: Palindrome Number

--- task4/app.js
+++ task4/app.js
@@ -3,11 +3,16 @@ let num = prompt("Enter a number: ");
 function isPalindrome(num) {
     num = Number(num); 
     if (num < 0) {
-       
         console.log("The number is not a palindrome.");
         let p = document.createElement("p");
         p.style.color = "red";
-        p.textContent = "Negetive number is not a palindrome.";
+        p.style.backgroundColor = "#ffe6e6";
+        p.style.padding = "10px";
+        p.style.fontSize = "18px";
+        p.style.borderRadius = "8px";
+        p.style.width = "fit-content";
+        p.style.margin = "10px auto";
+        p.textContent = "Negative number is not a palindrome.";
         document.body.appendChild(p);
         return;
     }
@@ -19,18 +24,23 @@ function isPalindrome(num) {
         reversed = reversed * 10 + digit;
         num = Math.floor(num / 10);
     }
+    let p = document.createElement("p");
+    p.style.padding = "10px";
+    p.style.fontSize = "18px";
+    p.style.borderRadius = "8px";
+    p.style.width = "fit-content";
+    p.style.margin = "10px auto";
     if (reversed === original) {
         console.log("The number is a palindrome.");
-        let p = document.createElement("p");
         p.style.color = "green";
+        p.style.backgroundColor = "#e6ffe6";
         p.textContent = "The number is a palindrome.";
-        document.body.appendChild(p);
     } else {
         console.log("The number is not a palindrome.");
-        let p = document.createElement("p");
         p.style.color = "red";
+        p.style.backgroundColor = "#ffe6e6";
         p.textContent = "The number is not a palindrome.";
-        document.body.appendChild(p);
     }
+    document.body.appendChild(p);
 }
 isPalindrome(num);
